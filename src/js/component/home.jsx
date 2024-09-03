315198ed7368fdc57dd1506115abde460dc9caa7
Daniel Montoya <153567770+mannydontoya@users.noreply.github.com>
2024-09-03 20:53:21 +0000

    Fixed counter to add 0s

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,19 +4,18 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = ({seconds}) => {
-	const numToString = seconds.toString()
-	// 9 
-	// 10 
+const Home = ({ seconds }) => {
+	const numToString = seconds.toString().padStart(6, "0");
+
 	return (
 		<div className="container-fluid bg-black">
 			<div className="icon text-light"><i class="far fa-clock"></i></div>
-			<div className="seconds6">{seconds < 10 ? seconds:numToString[1]}</div>
-			<div className="seconds5">{seconds < 10 ? seconds:numToString[1]}</div>
-			<div className="seonds4">{seconds < 10 ? seconds:numToString[1]}</div>
-			<div className="seconds3">{seconds < 10 ? seconds:numToString[1]}</div>
-			<div className="seconds2">{seconds < 10 ? 0:numToString[0]}</div>
-			<div className="seconds1">{seconds < 10 ? seconds:numToString[1]}</div>
+			<div className="seconds6">{numToString[0]}</div>
+			<div className="seconds5">{numToString[1]}</div>
+			<div className="seconds4">{numToString[2]}</div>
+			<div className="seconds3">{numToString[3]}</div>
+			<div className="seconds2">{numToString[4]}</div>
+			<div className="seconds1">{numToString[5]}</div>
 		</div>
 	);
 };

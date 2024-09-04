1b409adaaee6ab2a3fe5ef1b26165d63320f1759
Daniel Montoya <153567770+mannydontoya@users.noreply.github.com>
2024-09-04 12:06:04 +0000

    Final changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,19 +4,19 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = ({ seconds }) => {
+const Home = ({ seconds}) => {
 	const numToString = seconds.toString().padStart(6, "0");
 
 	return (
-		<div className="container-fluid bg-black">
-			<div className="icon text-light"><i class="far fa-clock"></i></div>
-			<div className="seconds6">{numToString[0]}</div>
-			<div className="seconds5">{numToString[1]}</div>
-			<div className="seconds4">{numToString[2]}</div>
-			<div className="seconds3">{numToString[3]}</div>
-			<div className="seconds2">{numToString[4]}</div>
-			<div className="seconds1">{numToString[5]}</div>
-		</div>
+			<div className="container-fluid bg-black">
+				<div className="icon text-light"><i className="far fa-clock"></i></div>
+				<div className="seconds6">{numToString[0]}</div>
+				<div className="seconds5">{numToString[1]}</div>
+				<div className="seconds4">{numToString[2]}</div>
+				<div className="seconds3">{numToString[3]}</div>
+				<div className="seconds2">{numToString[4]}</div>
+				<div className="seconds1">{numToString[5]}</div>
+			</div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,10 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 let seconds = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'))
 setInterval(() =>{
     seconds++
-    ReactDOM.createRoot(document.getElementById('app')).render(<Home seconds={seconds}/>);
+    root.render(<Home seconds = {seconds}/>);
 }, 1000);
    
 
